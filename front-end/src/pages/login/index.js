1548f525e702e51f0acb30d3253b36e48903f31a
yejf <490293266@qq.com>
2021-11-04 22:34:03 +0800

    update

--- front-end/src/pages/login/index.js
+++ front-end/src/pages/login/index.js
@@ -3,7 +3,7 @@ import styles from './index.module.less'
 
 class Login extends React.Component {
   render() {
-    return <div className={styles['login-wrap']}>Login</div>
+    return <div className={styles['login-wrap']}>Login2</div>
   }
 }
 
